4324acc83352ac39300fabe140c45195996db3a6
Felipe Freitas <soufreitas.felipe@gmail.com>
2023-09-29 12:14:45 -0300

    ajuste no lat long de São Paulo

--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -5,8 +5,8 @@ export const useWeatherService =()=>{
  
     const getWeather = async ()=>{
         //  receber lat e long
-        const lat = 23.5505;
-        const long = 46.6333;
+        const lat = -23.5505;
+        const long = -46.6333;
    
         // const resp = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`);
        
